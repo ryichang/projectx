3417aa01fed8e1109368a0ffdb4d288d4f1c9938
ryichang <ryichang@gmail.com>
2016-03-04 00:43:45 -0800

    Set up Server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+
+// require express framework and additional modules
+var express = require('express'),
+	app = express(),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose');
+
+// middleware
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect('mongodb://localhost/projectx');
+
+// signup route with placeholder response
+app.get('/signup', function(req, res){
+	res.send('signup coming soon');
+});
+
+// login route with placeholder response
+app.get('/login', function(req, res){
+	res.send('login coming soon');
+});
+
+// listen on port 3000
+app.listen(3000, function(){
+	console.log('server is alive on localhost:3000');
+});
