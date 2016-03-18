8e2e829401bee86f52f6449cf77e5789d266dda8
ryichang <ryichang@gmail.com>
2016-03-17 23:56:56 -0700

    Changed column size

--- public/js/particles.js
+++ public/js/particles.js
@@ -8,7 +8,7 @@ function launchParticlesJS(a, e) {
     },
     particles: {
       color: "#454545",
-      shape: "polygon",
+      shape: "circle",
       opacity: 1,
       size: 6,
       size_random: true,
@@ -431,7 +431,7 @@ window.particlesJS = function(d, c) {
   }
   var b = document.createElement("canvas");
   b.style.width = "100%";
-  b.style.height = "400px";
+  b.style.height = "450px";
   var a = document.getElementById(d).appendChild(b);
   if (a != null) {
     launchParticlesJS(d, c)
